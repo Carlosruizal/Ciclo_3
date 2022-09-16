996536ee612d8d7d66e51bfd303ef876e284f131
Carlosruizal <ruizac@uninorte.edu.co>
2022-09-15 19:01:52 -0500

    Conexion con Json

---
+++ VS_CODE_Ciclo_3/Primer_codigo_Ciclo_3/js/validaciones.js
@@ -0,0 +1,20 @@
+function validar_formulario(){
+
+var username=document.registrouser.username;
+var email=document.registrouser.correo;
+var passw=document.registrouser.passw;
+
+alert("Estoy ingresando a la funcion de validad")
+
+
+}
+
+function mensajenombre(){
+
+    alert("Estar en el campo nombre")
+}
+
+function mensajecorreo(){
+    alert("Estas en el Correo")
+}
+
